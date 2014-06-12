0ee3d6ee8e321d622accf87be49d03b91d781d3e
James Topham <apple@Apples-MacBook-Pro.local>
2014-06-12 13:39:12 +0100

    Initial Commit

---
+++ Brick Breaker/JTMyScene.h
@@ -0,0 +1,13 @@
+//
+//  JTMyScene.h
+//  Brick Breaker
+//
+
+//  Copyright (c) 2014 James Topham. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface JTMyScene : SKScene
+
+@end
---
+++ Brick BreakerTests/Brick_BreakerTests.m
@@ -0,0 +1,34 @@
+//
+//  Brick_BreakerTests.m
+//  Brick BreakerTests
+//
+//  Created by James Topham on 12/06/2014.
+//  Copyright (c) 2014 James Topham. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Brick_BreakerTests : XCTestCase
+
+@end
+
+@implementation Brick_BreakerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
